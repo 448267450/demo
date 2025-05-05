67043178a93482895df025be8b29061d81d7aa3d
ryanren <wenkun.ren@gmail.com>
2025-05-04 22:06:12 -0500

    map func

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,23 @@ import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
-let text = "Astral's Learning sessions";
+let data = [{
+    id: 1,
+    title: 'Welcome to this study' 
+}, {
+    id: 2,
+    title: 'Hope you can learn React well'
+}, {
+    id: 3,
+    title: 'Feel free to ask me any questions'
+}]
 root.render(
-    <div>
-        {text}
-        
-    </div>
+    <>
+        <h2>News Today</h2>
+        <ul>
+            {data.map((item) => {
+                return <li key={item.id}>{item.title}</li>
+            })}
+        </ul>
+    </>
 );
